ea4d6fa190dbd759345e58e6ea649a2c1b2fda6b
Delta-ReFz <el-mehdi.oukid.1@ens.etsmtl.ca>
2025-01-28 12:13:10 -0500

    Updating the navbar

--- portfoliov2/app/page.js
+++ portfoliov2/app/page.js
@@ -1,9 +1,13 @@
+'use client';
+
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     
-    <Navbar/>
+    <>
+      <Navbar />
+    </>
 
   );
 }
--- portfoliov2/components/Navbar.jsx
+++ portfoliov2/components/Navbar.jsx
@@ -5,10 +5,22 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-    <nav>
-        <a href="">
+    <nav className='flex justify-between items-center py-4 px-5 lg:px-8 xl:px-[8%] fixed w-full z-50 bg-white shadow-md'>
+        <a href="#top">
             <Image src={assets.logo} className='w-28 cursor-pointer mr-14' alt='Logo-name'/>
         </a>
+
+        <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3'>
+            <li><a href="#top"></a>Home</li>
+            <li><a href="#about"></a>About me</li>
+            <li><a href="#services"></a>Services</li>
+            <li><a href="#work"></a>My Work</li>
+            <li><a href="#contact"></a>Contact me</li>
+        </ul>
+
+        <div>
+            <a href="#contact">Contact <Image src={assets.arrow_icon} alt='contactimg' className='w-3'/></a>
+        </div>
     </nav></>
   )
 }
