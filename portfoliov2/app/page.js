ee322662b2e3ada3bb7cc6a4978fbe3230376efa
Delta-ReFz <el-mehdi.oukid.1@ens.etsmtl.ca>
2025-01-30 16:54:25 -0500

    added contact me & my resume button

--- portfoliov2/app/page.js
+++ portfoliov2/app/page.js
@@ -1,5 +1,6 @@
 'use client';
 
+import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     
     <>
       <Navbar />
+      <Header />  
     </>
 
   );
