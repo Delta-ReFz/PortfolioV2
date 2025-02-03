8a270d1127627f5b8fd8ad2d75c7ea1d97eea315
Delta-ReFz <el-mehdi.oukid.1@ens.etsmtl.ca>
2025-02-02 23:15:15 -0500

    Update Contact.jsx

--- portfoliov2/components/Contact.jsx
+++ portfoliov2/components/Contact.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <div> 
-            <h4 className='text-center mb-2 text-lg font-Ovo'>My portfolio</h4>
-            <h2 className='text-center text-5xl font-Ovo'>My latest projects</h2>
+       
+       <div id='#contact' className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat bg-center bg-[length:90%_auto]'> 
+            <h4 className='text-center mb-2 text-lg font-Ovo'>Connect with me</h4>
+            <h2 className='text-center text-5xl font-Ovo'>Get in touch</h2>
             <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
-                Welcome to my e-Portfolio! Here are some of the projects I have worked on recently,
-                you can find the rest on my GitHub profile.
+                I'd love to hear from you! If you have any questions or would like to work together, feel free to contact me.
             </p>
         </div>
     )
