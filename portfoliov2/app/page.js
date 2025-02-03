cee5ac4bfb97a9801cf836da8087347d6ed44399
Delta-ReFz <el-mehdi.oukid.1@ens.etsmtl.ca>
2025-02-02 23:52:44 -0500

    Added links github,linkedin

--- portfoliov2/app/page.js
+++ portfoliov2/app/page.js
@@ -2,6 +2,7 @@
 
 import About from "@/components/About";
 import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 import Projects from "@/components/Projects";
@@ -17,6 +18,7 @@ export default function Home() {
       <Work />
       <Projects />
       <Contact />
+      <Footer />
     </>
 
   );
---
+++ portfoliov2/components/Footer.jsx
@@ -0,0 +1,29 @@
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='mt-20'>
+        <div className='text-center'>
+            <Image src={assets.un} alt='' className='w-36 mx-auto mb-2' />
+
+            <div className='w-max flex items-center mx-auto gap-2'>
+            <Image src={assets.mail_icon} alt='' className='w-6' />
+            mehdioukid001@gmail.com
+            </div>
+        </div>
+        <div className='tetx-center mt-12 sm:flex items-center justify-between border-t border-gray-400 mx-[10%] py-6'>
+            <p>© 2025 El Mehdi Oukid. All rights reserved.</p>
+            <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
+                {/* Blank sert a ouvrire un nouvel onglet*/}
+                <li><a target='_blank' href="https://github.com/delta-refz">GitHub</a></li>
+                <li><a target='_blank' href="https://linkedin.com/in/el-mehdi-oukid-9873b3334">LinkedIn</a></li>
+            </ul>
+        </div>
+
+    </div>
+  )
+}
+
+export default Footer
