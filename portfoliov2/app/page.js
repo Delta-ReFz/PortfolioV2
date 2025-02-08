936993b77d038fed67bd3c4d942f39695ad220d7
Delta-ReFz <el-mehdi.oukid.1@ens.etsmtl.ca>
2025-02-08 01:16:48 -0500

    Dark Mode

--- portfoliov2/app/page.js
+++ portfoliov2/app/page.js
@@ -1,5 +1,6 @@
 'use client';
 
+import { useEffect, useState } from "react";
 import About from "@/components/About";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
@@ -7,18 +8,39 @@ import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 import Projects from "@/components/Projects";
 import Work from "@/components/Work";
+import { useFormState } from "react-dom";
 
 export default function Home() {
-  return (
-    
+
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    if(localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      setIsDarkMode(true);
+    }else {
+      setIsDarkMode(false);
+    }
+  }, [])
+
+  useEffect(()=> {
+    if(isDarkMode) {
+      document.documentElement.classList.add('dark');
+      localStorage.theme = 'dark';
+    }else {
+      document.documentElement.classList.remove('dark');
+      localStorage.theme = '';
+    }
+  }, [isDarkMode])
+
+  return (  
     <>
-      <Navbar />
-      <Header />  
-      <About />
-      <Work />
-      <Projects />
-      <Contact />
-      <Footer />
+      <Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+      <Header isDarkMode={isDarkMode}/>  
+      <About isDarkMode={isDarkMode}/>
+      <Work isDarkMode={isDarkMode}/>
+      <Projects isDarkMode={isDarkMode}/>
+      <Contact isDarkMode={isDarkMode}/>
+      <Footer isDarkMode={isDarkMode}/>
     </>
 
   );
--- portfoliov2/components/Navbar.jsx
+++ portfoliov2/components/Navbar.jsx
@@ -2,7 +2,7 @@ import {assets} from '@/assets/assets'
 import Image from 'next/image'
 import React, {useEffect, useRef, useState} from 'react'
 
-const Navbar = () => {
+const Navbar = ({isDarkMode, setIsDarkMode}) => {
 
     const [isScroll, setIsScroll] = useState(false);
     const sideMenuRef = useRef();
@@ -29,14 +29,14 @@ const Navbar = () => {
 
   return (
 <>
-    <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
+    <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%] dark:hidden'>
         <Image src={assets.header_bg_color} alt='' className='w-full' />
     </div>
 
-    <nav className={`flex justify-between items-center py-4 px-5 lg:px-8 xl:px-[8%] fixed w-full z-50 ${isScroll ? "bg-white shadow-sm bg-opacity-50 backdrop-blur-lg" : ""}`}>
+    <nav className={`flex justify-between items-center py-4 px-5 lg:px-8 xl:px-[8%] fixed w-full z-50 ${isScroll ? "bg-white shadow-sm bg-opacity-50 backdrop-blur-lg dark:bg-darkTheme dark:shadow-white/20" : ""}`}>
        
         <a href="#top">
-            <Image src={assets.un} className='w-28 cursor-pointer mr-14' alt='Logo-name'/>
+            <Image src={isDarkMode ? assets.un : assets.un} className='w-28 cursor-pointer mr-14' alt='Logo-name'/>
         </a>
 
         <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isScroll ? "" : "bg-white shadow-sm bg-opacity-50"} `}>
@@ -48,9 +48,11 @@ const Navbar = () => {
         </ul>
 
         <div className='flex items-center gap-4'>
-            <button>
-                <Image src={assets.moon_icon} alt='' className='w-6' />
+            
+            <button onClick={() => setIsDarkMode(prev => !prev)}> 
+                <Image src={isDarkMode ? assets.sun_icon : assets.moon_icon} alt='' className='w-6' />
             </button>
+
             <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo'>
                 Contact 
                 <Image src={assets.arrow_icon} alt='contactimg' className='w-3'/>
